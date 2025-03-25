77a50a59206cd039b2655de04076f8c03d095d96
Axronl7 <Aaronliu657@gmail.com>
2025-03-25 10:16:56 -0400

    . 3/25

--- flashcards.py
+++ flashcards.py
@@ -16,3 +16,22 @@ flash_cards = [
 cards_data = [flash_Card.to_dict() for flash_Card in flash_cards]
 with open("flashcards.json", "w") as file:
     json.dump(cards_data, file, indent=4)
+
+
+
+    class teacher(flash_Card):
+        def __init__(self, question, answer, user):
+            super().__init__(question, answer)
+            self.user = user
+""" try:
+    with open("cars.json", "r") as file:
+        cars_data = json.load(file)
+except FileNotFoundError:
+    cars_data = []
+
+cards_data.append(new_card.to_dict())
+
+with open("cars.json", "r") as file:
+    json.dump(cards_data, file, indent=4) """
+
+    class Students(flash_Card):
