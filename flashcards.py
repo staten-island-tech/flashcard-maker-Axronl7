7794bc2fb454906ef9487f1ecb7c38acdc3750c7
Axronl7 <Aaronliu657@gmail.com>
2025-04-04 10:11:17 -0400

    4/4 finallllllyyyyyyyy

--- flashcards.py
+++ flashcards.py
@@ -19,72 +19,65 @@ with open("flashcards.json", "w") as file:
     json.dump(cards_data, file, indent=4)
 
 
-
+mode = input("Student or TeacherMODE: input s or t")
+if mode.lower() == "t":
+    print("teacher mode")
     class teacher(flash_Card):
         def __init__(self, question, answer, user):
             super().__init__(question, answer)
             self.user = user
-
-
-ask1 = "do you want to continue: yes/no"
-askB = input(ask1)
-while askB.lower() == "yes":
-    questionA = input("input a question:")
-    answerA = input(f"input the answer to {questionA}:")
-    print(input(ask1))
-    new_card = flash_Card(questionA, answerA)                
-    askB = input(f"{ask1}")
-    if askB.lower() == "no":
-        print("Goodbye")
-        break
-    
-
-
-try:
-    with open("flashcards.json", "r") as file:
-        cards_data = json.load(file)
-except FileNotFoundError:
-    cards_data = []
-
-cards_data.append(new_card.to_dict())
-
-with open("flashcards.json", "w") as file:
-    json.dump(cards_data, file, indent=4)
-
-
-class Students(flash_Card):
-    def __init__(self, question, answer, streak):
-        super().__init__(question, answer)
-        self.streak = streak
-    
-    mode = input("Student or TeacherMODE: input s or t")
-    if mode.lower() == "s":
-        print("student mode")
-        
-
-        ask = "do you want to continue: yes/no"
-        askA = input(f"{ask}")
-        streak = 0
-        points = 0 
-
-
-    while askA.lower() == "yes":      
-        question = random.choice(cards_data)
-        answer =  input(f"{question['question']} answer:")
-        if streak == 5:
-            points += 2
-        
-        if answer.lower() == question['answer'].lower():
-            streak += 1
-            points += 1
-            print(input(f"correct, {ask}           points:{points}     streak:{streak}"))
-        
-        else:
-            print(f"incorrect the correct answer is {question['answer']}     points:{points}     streak:{streak}")
-            streak = 0
             
-        askA = input(f"{ask}")
-        if askA.lower() == "no":
-            print("Goodbye")
-            break
+        ask1 = "do you want to continue: yes/no"
+        askB = input(ask1)
+        while askB.lower() == "yes":
+            questionA = input("input a question:")
+            answerA = input(f"input the answer to {questionA}:")
+            new_card = flash_Card(questionA, answerA)                
+            
+            askB = input(f"{ask1}")
+            if askB.lower() == "no":
+                print("Goodbye")
+                break
+        try:
+            with open("flashcards.json", "r") as file:
+                cards_data = json.load(file)
+        except FileNotFoundError:
+            cards_data = []
+
+        cards_data.append(new_card.to_dict())
+
+        with open("flashcards.json", "w") as file:
+            json.dump(cards_data, file, indent=4)
+
+else:
+        print("student mode")
+        class Students(flash_Card):
+            def __init__(self, question, answer, streak):
+                super().__init__(question, answer)
+                self.streak = streak
     
+            ask = "do you want to continue: yes/no"
+            askA = input(f"{ask}")
+            streak = 0
+            points = 0 
+
+
+            while askA.lower() == "yes":      
+                question = random.choice(cards_data)
+                answer =  input(f"{question['question']} answer:")
+                if streak == 5:
+                    points += 2
+        
+                if answer.lower() == question['answer'].lower():
+                    streak += 1
+                    points += 1
+                    print(input(f"correct, {ask}           points:{points}     streak:{streak}"))
+        
+                else:
+                    print(f"incorrect the correct answer is {question['answer']}     points:{points}     streak:{streak}")
+                    streak = 0
+            
+                askA = input(f"{ask}")
+                if askA.lower() == "no":
+                    print("Goodbye")
+                    break
